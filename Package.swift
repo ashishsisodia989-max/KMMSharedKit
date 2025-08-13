4a3d081d80c5e66e11be01e566f80065e6682caa
Ashish Sisodia <ashish.sisodia@hcl-software.com>
2025-08-13 17:29:42 +0530

    Add Swift Package to distribution

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "KMMSharedKit",
-            url: "https://github02.hclpnp.com/ashish-sisodia/KMMSharedKit/releases/download/1.0.0/KMMSharedKit.xcframework.zip",
+            url: "https://github.com/ashishsisodia989-max/KMMSharedKit/releases/download/1.0.0/KMMSharedKit.xcframework.zip",
             checksum: "1fe383e3b37313d25d53cbf8815a97461c7dc326af45c9259b7a364b19f894a9"
         )
     ]
